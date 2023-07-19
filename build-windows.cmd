153858848be4e580f1dce1ff9ea1f6e00d55740b
xbotter <xbotter@live.cn>
2023-07-19 11:04:57 +0800

    fix: result view

--- build-windows.cmd
+++ build-windows.cmd
@@ -1,1 +1,1 @@
-dotnet publish -c Release -o publish/windows --self-contained true -r win10-x64 -p:PublishSingleFile=true .\PromptPlayground.Desktop\PromptPlayground.Desktop.csproj
+dotnet publish -c Release -o publish/windows --self-contained true -r win-x64 -p:PublishSingleFile=true .\PromptPlayground.Desktop\PromptPlayground.Desktop.csproj
