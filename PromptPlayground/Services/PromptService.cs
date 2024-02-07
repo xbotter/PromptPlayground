4a6bef349369fa6cbadfae49f6f9097e4224808a
xbotter <xbotter@live.cn>
2024-02-07 18:14:08 +0800

    dotnet format

--- PromptPlayground/Services/PromptService.cs
+++ PromptPlayground/Services/PromptService.cs
@@ -10,56 +10,55 @@ using System.Threading.Tasks;
 
 namespace PromptPlayground.Services
 {
-    public class PromptService
-    {
-        private readonly Kernel _kernel;
-        private readonly ILLMConfigViewModel model;
+	public class PromptService
+	{
+		private readonly Kernel _kernel;
+		private readonly ILLMConfigViewModel model;
 
-        public PromptService(IConfigAttributesProvider provider)
-        {
-            this.model = provider.GetLLM() ?? throw new Exception("无法创建Kernel，请检查LLM配置");
+		public PromptService(IConfigAttributesProvider provider)
+		{
+			this.model = provider.GetLLM() ?? throw new Exception("无法创建Kernel，请检查LLM配置");
 
-            var builder = model.CreateKernelBuilder();
+			var builder = model.CreateKernelBuilder();
 
 
-            _kernel = builder.Build();
+			_kernel = builder.Build();
 
-            _kernel.ImportPluginFromType<TimePlugin>();
+			_kernel.ImportPluginFromType<TimePlugin>();
 
-        }
+		}
 
-        public async Task<GenerateResult> RunAsync(string prompt, PromptExecutionSettings? config, KernelArguments arguments, CancellationToken cancellationToken = default)
-        {
-            var sw = Stopwatch.StartNew();
-            var func = _kernel.CreateFunctionFromPrompt(prompt, config);
+		public async Task<GenerateResult> RunAsync(string prompt, PromptExecutionSettings? config, KernelArguments arguments, CancellationToken cancellationToken = default)
+		{
+			var sw = Stopwatch.StartNew();
+			var func = _kernel.CreateFunctionFromPrompt(prompt, config);
 
-            var result = await func.InvokeAsync(_kernel, arguments);
-            sw.Stop();
-            try
-            {
-                var usage = model.GetUsage(result);
-                return new GenerateResult()
-                {
-                    Text = result.GetValue<string>() ?? "",
-                    TokenUsage = usage,
-                    Elapsed = sw.Elapsed,
+			var result = await func.InvokeAsync(_kernel, arguments);
+			sw.Stop();
+			try
+			{
+				var usage = model.GetUsage(result);
+				return new GenerateResult()
+				{
+					Text = result.GetValue<string>() ?? "",
+					TokenUsage = usage,
+					Elapsed = sw.Elapsed
+				};
 
-                };
+			}
+			catch (KernelException ex)
+			{
+				return new GenerateResult()
+				{
+					Text = ex.Message,
+					Elapsed = sw.Elapsed,
+					Error = ex.Message,
+				};
+			}
+		}
 
-            }
-            catch (KernelException ex)
-            {
-                return new GenerateResult()
-                {
-                    Text = ex.Message,
-                    Elapsed = sw.Elapsed,
-                    Error = ex.Message,
-                };
-            }
-        }
-
-        public KernelArguments CreateArguments() => new KernelArguments();
-    }
+		public KernelArguments CreateArguments() => new KernelArguments();
+	}
 
 
 }
