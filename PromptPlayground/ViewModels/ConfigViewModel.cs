ab30418c59caa8433fe8ebb1c522caaae2662ae3
xbotter <xbotter@live.cn>
2024-02-18 16:15:55 +0800

    Due to issue https://github.com/SciSharp/LLamaSharp/issues/382, support for llamaSharp has been temporarily removed.

--- PromptPlayground/ViewModels/ConfigViewModel.cs
+++ PromptPlayground/ViewModels/ConfigViewModel.cs
@@ -17,147 +17,147 @@ using System.Text.Json.Serialization;
 namespace PromptPlayground.ViewModels;
 
 public partial class ConfigViewModel : ViewModelBase, IConfigAttributesProvider,
-                                        IRecipient<ConfigurationRequestMessage>,
-                                        IRecipient<RequestMessage<IConfigAttributesProvider>>
+										IRecipient<ConfigurationRequestMessage>,
+										IRecipient<RequestMessage<IConfigAttributesProvider>>
 {
-    private string[] RequiredAttributes =
+	private string[] RequiredAttributes =
    [
        #region LLM Config
        ConfigAttribute.AzureDeployment,
-       ConfigAttribute.AzureEndpoint,
-       ConfigAttribute.AzureSecret,
-       ConfigAttribute.BaiduClientId,
-       ConfigAttribute.BaiduSecret,
-       ConfigAttribute.BaiduModel,
-       ConfigAttribute.OpenAIApiKey,
-       ConfigAttribute.OpenAIModel,
-       ConfigAttribute.DashScopeApiKey,
-       ConfigAttribute.DashScopeModel,
-       ConfigAttribute.LlamaModelPath
+	   ConfigAttribute.AzureEndpoint,
+	   ConfigAttribute.AzureSecret,
+	   ConfigAttribute.BaiduClientId,
+	   ConfigAttribute.BaiduSecret,
+	   ConfigAttribute.BaiduModel,
+	   ConfigAttribute.OpenAIApiKey,
+	   ConfigAttribute.OpenAIModel,
+	   ConfigAttribute.DashScopeApiKey,
+	   ConfigAttribute.DashScopeModel,
+	   ConfigAttribute.LlamaModelPath
        #endregion
    ];
 
-    public List<ConfigAttribute> AllAttributes { get; set; } = [];
+	public List<ConfigAttribute> AllAttributes { get; set; } = [];
 
-    [ObservableProperty]
-    private int maxCount = 3;
+	[ObservableProperty]
+	private int maxCount = 3;
 
-    [ObservableProperty]
-    private bool runStream = false;
+	[ObservableProperty]
+	private bool runStream = false;
 
-    #region Model
-    private int modelSelectedIndex = 0;
-    private ProfileService<ConfigViewModel> _profile;
+	#region Model
+	private int modelSelectedIndex = 0;
+	private ProfileService<ConfigViewModel> _profile;
 
-    public int ModelSelectedIndex
-    {
-        get => modelSelectedIndex; set
-        {
-            if (modelSelectedIndex != value)
-            {
-                modelSelectedIndex = value;
-                OnPropertyChanged(nameof(ModelSelectedIndex));
-                OnPropertyChanged(nameof(SelectedModel));
-                OnPropertyChanged(nameof(ModelAttributes));
-                OnPropertyChanged(nameof(SelectedModel.Name));
-            }
-        }
-    }
+	public int ModelSelectedIndex
+	{
+		get => modelSelectedIndex; set
+		{
+			if (modelSelectedIndex != value)
+			{
+				modelSelectedIndex = value;
+				OnPropertyChanged(nameof(ModelSelectedIndex));
+				OnPropertyChanged(nameof(SelectedModel));
+				OnPropertyChanged(nameof(ModelAttributes));
+				OnPropertyChanged(nameof(SelectedModel.Name));
+			}
+		}
+	}
 
-    [JsonIgnore]
-    public List<string> ModelLists => LLMs.Select(_ => _.Name).ToList();
-    [JsonIgnore]
-    public IList<ConfigAttribute> ModelAttributes => SelectedModel.SelectAttributes(this.AllAttributes);
-    [JsonIgnore]
-    public ILLMConfigViewModel SelectedModel => LLMs[ModelSelectedIndex];
-    [JsonIgnore]
-    private readonly List<ILLMConfigViewModel> LLMs = [];
-    #endregion
+	[JsonIgnore]
+	public List<string> ModelLists => LLMs.Select(_ => _.Name).ToList();
+	[JsonIgnore]
+	public IList<ConfigAttribute> ModelAttributes => SelectedModel.SelectAttributes(this.AllAttributes);
+	[JsonIgnore]
+	public ILLMConfigViewModel SelectedModel => LLMs[ModelSelectedIndex];
+	[JsonIgnore]
+	private readonly List<ILLMConfigViewModel> LLMs = [];
+	#endregion
 
-    #region IConfigAttributesProvider
-    IList<ConfigAttribute> IConfigAttributesProvider.AllAttributes => this.AllAttributes;
-    public ILLMConfigViewModel GetLLM()
-    {
-        return this.SelectedModel;
-    }
-    #endregion
+	#region IConfigAttributesProvider
+	IList<ConfigAttribute> IConfigAttributesProvider.AllAttributes => this.AllAttributes;
+	public ILLMConfigViewModel GetLLM()
+	{
+		return this.SelectedModel;
+	}
+	#endregion
 
-    public ConfigViewModel(bool requireLoadConfig = false) : this()
-    {
-        if (requireLoadConfig)
-        {
-            WeakReferenceMessenger.Default.RegisterAll(this);
-            LoadConfigFromUserProfile();
-        }
-    }
+	public ConfigViewModel(bool requireLoadConfig = false) : this()
+	{
+		if (requireLoadConfig)
+		{
+			WeakReferenceMessenger.Default.RegisterAll(this);
+			LoadConfigFromUserProfile();
+		}
+	}
 
-    public ConfigViewModel()
-    {
-        this.AllAttributes = CheckAttributes(this.AllAttributes);
+	public ConfigViewModel()
+	{
+		this.AllAttributes = CheckAttributes(this.AllAttributes);
 
-        LLMs.Add(new AzureOpenAIConfigViewModel(this));
-        LLMs.Add(new BaiduConfigViewModel(this));
-        LLMs.Add(new OpenAIConfigViewModel(this));
-        LLMs.Add(new DashScopeConfigViewModel(this));
-        LLMs.Add(new LlamaSharpConfigViewModel(this));
+		LLMs.Add(new AzureOpenAIConfigViewModel(this));
+		LLMs.Add(new BaiduConfigViewModel(this));
+		LLMs.Add(new OpenAIConfigViewModel(this));
+		LLMs.Add(new DashScopeConfigViewModel(this));
+		// LLMs.Add(new LlamaSharpConfigViewModel(this));
 
-        this._profile = new ProfileService<ConfigViewModel>("user.config");
-    }
+		this._profile = new ProfileService<ConfigViewModel>("user.config");
+	}
 
-    private void LoadConfigFromUserProfile()
-    {
-        var vm = this._profile.Get();
-        if (vm != null)
-        {
-            this.AllAttributes = CheckAttributes(vm.AllAttributes);
+	private void LoadConfigFromUserProfile()
+	{
+		var vm = this._profile.Get();
+		if (vm != null)
+		{
+			this.AllAttributes = CheckAttributes(vm.AllAttributes);
 
-            this.MaxCount = vm.MaxCount;
-            this.RunStream = vm.RunStream;
-            this.ModelSelectedIndex = vm.ModelSelectedIndex;
-        }
-    }
-    private List<ConfigAttribute> CheckAttributes(List<ConfigAttribute> list)
-    {
-        foreach (var item in RequiredAttributes)
-        {
-            if (!list.Any(_ => _.Name == item))
-            {
-                list.Add(new ConfigAttribute(item));
-            }
-        }
-        return list;
-    }
+			this.MaxCount = vm.MaxCount;
+			this.RunStream = vm.RunStream;
+			this.ModelSelectedIndex = vm.ModelSelectedIndex;
+		}
+	}
+	private List<ConfigAttribute> CheckAttributes(List<ConfigAttribute> list)
+	{
+		foreach (var item in RequiredAttributes)
+		{
+			if (!list.Any(_ => _.Name == item))
+			{
+				list.Add(new ConfigAttribute(item));
+			}
+		}
+		return list;
+	}
 
-    private void SaveConfigToUserProfile()
-    {
-        this._profile.Save(this);
-    }
+	private void SaveConfigToUserProfile()
+	{
+		this._profile.Save(this);
+	}
 
-    public void SaveConfig()
-    {
-        SaveConfigToUserProfile();
-    }
+	public void SaveConfig()
+	{
+		SaveConfigToUserProfile();
+	}
 
-    public void ReloadConfig()
-    {
-        this.LoadConfigFromUserProfile();
-    }
+	public void ReloadConfig()
+	{
+		this.LoadConfigFromUserProfile();
+	}
 
-    public void Receive(RequestMessage<IConfigAttributesProvider> message)
-    {
-        message.Reply(this);
-    }
+	public void Receive(RequestMessage<IConfigAttributesProvider> message)
+	{
+		message.Reply(this);
+	}
 
-    public void Receive(ConfigurationRequestMessage request)
-    {
-        switch (request.Config)
-        {
-            case nameof(MaxCount):
-                request.Reply(this.MaxCount.ToString());
-                break;
-            case nameof(RunStream):
-                request.Reply(this.RunStream.ToString());
-                break;
-        }
-    }
+	public void Receive(ConfigurationRequestMessage request)
+	{
+		switch (request.Config)
+		{
+			case nameof(MaxCount):
+				request.Reply(this.MaxCount.ToString());
+				break;
+			case nameof(RunStream):
+				request.Reply(this.RunStream.ToString());
+				break;
+		}
+	}
 }
