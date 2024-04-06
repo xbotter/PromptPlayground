310e341dfacf3aac9c67a5f265d40242beecb77d
xbotter <xbotter@users.noreply.github.com>
2024-04-06 21:59:04 +0800

    feat: toggle theme in about view (#44)

--- PromptPlayground/Views/AboutView.axaml.cs
+++ PromptPlayground/Views/AboutView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Styling;
 
 namespace PromptPlayground.Views
 {
@@ -14,6 +15,16 @@ namespace PromptPlayground.Views
         public void CloseClick(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void ToggleTheme(object sender, RoutedEventArgs e)
+        {
+            var app = Application.Current;
+            if (app is not null)
+            {
+                var theme = app.ActualThemeVariant;
+                app.RequestedThemeVariant = theme == ThemeVariant.Dark ? ThemeVariant.Light : ThemeVariant.Dark;
+            }
         }
     }
 }
